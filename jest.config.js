d5e52cb04104a679cf4be28ff9e8fca4ffb256b8
Jared Rethman <jaredrethman@gmail.com>
2019-09-23 15:53:13 +0200

    JEST: Adding jest.config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	setupFilesAfterEnv: [
+		'./node_modules/jest-enzyme/lib/index.js',
+		'<rootDir>assets/js/tests/setup.js' ],
+	testPathIgnorePatterns: [
+		'/node_modules/',
+		'<rootDir>/webpack/'
+	],
+	transform: {
+		'^.+\\.js$': 'babel-jest',
+	},
+	preset: '@wordpress/jest-preset-default',
+	moduleNameMapper: {
+		'@wordpress\\/(blocks|components|date|editor|element|data|utils|edit-post|viewport|plugins|core-data)': '<rootDir>/node_modules/@wordpress/$1',
+	},
+	moduleDirectories: ['node_modules', 'assets/js' ],
+};
